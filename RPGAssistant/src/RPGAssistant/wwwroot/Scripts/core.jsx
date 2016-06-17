ba9737f554cd8799455103c4cafdd60e8b60a32b
RobertLaverick <robert.laverick@bedegaming.com>
2016-06-17 17:51:40 +0100

    Initial Work
    Unable to run due to missing assembly issue.
    
    Next step is to experiment with React.JS - try just a basic character sheet from an API

---
+++ RPGAssistant/src/RPGAssistant/wwwroot/Scripts/core.jsx
@@ -0,0 +1,7 @@
+﻿var commentBox = React.createClass({
+    render: function() {
+        return (<div className="commentBox">Hello World, I am a CommentBox.</div>);
+    }
+});
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
